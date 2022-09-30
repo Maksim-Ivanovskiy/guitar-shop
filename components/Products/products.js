951303f1d98832a20719fe13f478a329b4305b22
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-09-30 17:55:02 -0400

    map method

--- components/Products/products.js
+++ components/Products/products.js
@@ -27,3 +27,31 @@ class Products {
 
 const productsPage = new Products();
 productsPage.render();
+
+
+
+/* const createProductsLayout = async () => {
+    const products = await getProducts();
+    
+    const productsHTML = products
+        .map(product => `
+            <li>
+                <span>${product.title}</span>
+                <img src="${product.image}"/>
+                <span>${product.price}</span>
+                <button>Add to the cart</button>
+            </li>
+        `)
+        .join('')
+    document.querySelector('.products').innerHTML = productsHTML
+};
+
+const getProducts = async () => {
+    const response = await fetch('https://fakestoreapi.com/products')
+    const products = await response.json();
+    console.log({ products })
+    return products
+};
+
+
+createProductsLayout() */

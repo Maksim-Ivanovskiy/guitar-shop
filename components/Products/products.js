7186e48b0fd52299e5aecfd0cfce836fe12c9875
Maksim Ivanovskiy <ivanovskiy.maxim@mail.ru>
2022-10-05 21:53:55 -0400

    new styles for btn and name

--- components/Products/products.js
+++ components/Products/products.js
@@ -4,10 +4,10 @@ class Products {
         let htmlCatalog = '';
         CATALOG.forEach(({id, name, price, img}) => {
             htmlCatalog += `
-                <li class="products-container">
+                <li class="products-element">
                     <span class="products-element__name">${name}</span>
                     <img class="products-element__img" src="${img}"/>
-                    <span class="products-element__price">${price}</span>
+                    <span class="products-element__price">⚡️ ${price.toLocaleString()} USD</span>
                     <button class="products-element__btn">Add to the cart</button>
                 </li>
             `;
